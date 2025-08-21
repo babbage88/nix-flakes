e9f5dd46084eecf1be0f465c6f065deb1856aefe
Justin Trahan <justin@trahan.dev>
2025-08-21 14:25:13 +0000

    Updated install script to include full source path

--- rockydev2.homemgr/install.sh
+++ rockydev2.homemgr/install.sh
@@ -4,8 +4,8 @@ HMDIR="$HOME/.config/home-manager"
 NIXHOMECFG="$NIXSRCDIR/home.nix"
 export FLAKEKEY="$HMDIR/#jtrahan"
 printf "copying %s to %s...\n" "$NIXHOMECFG" "$HMDIR"
-cp home.nix $HMDIR/home.nix
+cp $NIXHOMECFG $HMDIR/home.nix
 printf "copying flake files to %s...\n" "$HMDIR"
-cp flake.* $HMDIR/
+cp $NIXSRCDIR/flake.* $HMDIR/
 printf "rebuild nix hm config: %s...\n" "$FLAKEKEY"
 home-manager switch --flake $FLAKEKEY -b backup
