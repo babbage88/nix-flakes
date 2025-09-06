da263a31785a4b0cd93dfefc3dd5619fe73afb8f
Justin Trahan <justin@trahan.dev>
2025-09-06 14:03:27 +0000

    nix-channel -add helper script

---
+++ ncadd.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+nix-channel --add https://nixos.org/channels/nixpkgs-unstable
+nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager
+nix-channel --update
