f009177b3d4509f3b18ec3a6f029933159212015
Justin Trahan <justin@trahan.dev>
2026-02-16 13:21:37 +0000

    installscript

--- rockydev_homemgr/home.sync-conflict-20251129-174218-7A66IHS.nix
+++ rockydev_homemgr/home.sync-conflict-20251129-174218-7A66IHS.nix
@@ -53,7 +53,6 @@
     pkgs.fastfetch
     pkgs.bat
     pkgs.bind
-    pkgs.nmap
     # # It is sometimes useful to fine-tune packages, for example, by applying
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
--- rockydev_homemgr/install.sh
+++ rockydev_homemgr/install.sh
@@ -3,9 +3,52 @@ NIXSRCDIR="$HOME/projects/nix-flakes/rockydev_homemgr"
 HMDIR="$HOME/.config/home-manager"
 NIXHOMECFG="$NIXSRCDIR/home.nix"
 export FLAKEKEY="$HMDIR/#jtrahan"
-printf "copying %s to %s...\n" "$NIXHOMECFG" "$HMDIR"
-cp $NIXHOMECFG $HMDIR/home.nix
-printf "copying flake files to %s...\n" "$HMDIR"
-cp $NIXSRCDIR/flake.* $HMDIR/
-printf "rebuild nix hm config: %s...\n" "$FLAKEKEY"
-home-manager switch --flake $FLAKEKEY -b backup
+
+install_hm() {
+	local srcdir=${0:-$NIXSRCDIR}
+	local dsthomedir=${1:-$HMDIR}
+	local src_hm_cfg=${2:-$NIXHOMECFG}
+	export FLAKEKEY="$dsthomedir/#$USER"
+
+	mkdir -p $dsthomedir
+	printf "copying %s to %s...\n" "$src_hm_cfg" "$dsthomedir"
+	cp $src_hm_cfg $dsthomedir/home.nix
+	printf "copying flake files to %s...\n" "$dsthomedir"
+	cp $srcdir/flake.* $dsthomedir/
+	printf "rebuild nix hm config: %s...\n" "$FLAKEKEY"
+	home-manager switch --flake $FLAKEKEY -b backup
+}
+
+update_nixpkg() {
+	local dsthomedir=${0:-$HMDIR}
+	local flake_key="$dsthomedir/#$USER"
+	local cur_dir_start=$(pwd)
+	printf "cd into home-manager install dir %s...\n" "$dsthomedir"
+	cd $dsthomedir
+	printf "Updating cix channels...\n"
+	nix-channel --update
+	printf "Updating nix flake.lock file...\n"
+	nix flake update
+	printf "switching home-manager to flake: %s\n" "$flake_key"
+	#home-manager switch --flake $flake_key
+	echo $flake_key
+	printf "changing directory back to the starting dir... %s\n" "$cur_dir_start"
+	cd $cur_dir_start
+}
+
+hm_switch() {
+	local dsthomedir=$HMDIR
+	local flake_key="$dsthomedir/#$USER"
+	local cur_dir_start=$(pwd)
+
+	export FLAKEKEY=$flake_key
+	echo "flake key is: $FLAKEKEY"
+	printf "cd into home-manager install dir %s...\n" "$dsthomedir"
+	cd $dsthomedir
+	printf "switching home-manager to flake: %s\n" "$flake_key"
+	home-manager switch --flake $flake_key
+	printf "changing directory back to the starting dir... %s\n" "$cur_dir_start"
+	cd $cur_dir_start
+}
+hm_switch
+#install_hm
