39ac4acf7f67f3da9051d4d634ebd697eb7403e5
Justin Trahan <justin@trahan.dev>
2026-02-17 02:03:02 +0000

    Finished adding ansi colors to install.sh

--- rockydev_homemgr/install.sh
+++ rockydev_homemgr/install.sh
@@ -4,15 +4,35 @@ HMDIR="$HOME/.config/home-manager"
 NIXHOMECFG="$NIXSRCDIR/home.nix"
 export FLAKEKEY="$HMDIR/#jtrahan"
 
+##### ANSI Color Codes #####
+C_B_RED='\033[31;1m'
+C_B_GREEN='\033[32;1m'
+C_B_YELLOW='\033[33;1m'
+C_B_BLUE='\033[34;1m'
+C_B_CYAN='\033[36;1m'
+C_B_WHITE='\033[37;1m'
+
+C_RED='\033[31m'
+C_GREEN='\033[32m'
+C_YELLOW='\033[33m'
+C_BLUE='\033[34m'
+C_CYAN='\033[36m'
+BOLD_WHITE='\033[01;97m'
+BOLD_GREEN='\033[01;96m'
+BOLD_BLUE='\033[01;94m'
+BOLD_YELLOW='\033[01;93m'
+BOLD_RED='\033[01;92m'
+C_RESET='\033[0m'
+
 install_hm() {
 	local srcdir=${1:-$NIXSRCDIR}
 	local dsthomedir=${2:-$HMDIR}
 	local src_hm_cfg=${3:-$NIXHOMECFG}
 
 	mkdir -p $dsthomedir
-	printf "copying %s to %s...\n" "$src_hm_cfg" "$dsthomedir"
+	printf "${BOLD_WHITE}copying %s to %s...\\n${C_RESET}" "$src_hm_cfg" "$dsthomedir"
 	cp $src_hm_cfg $dsthomedir/home.nix
-	printf "copying flake files to %s...\n" "$dsthomedir"
+	printf "${BOLD_WHITE}copying flake files to${C_CYAN} %s${BOLD_WHITE}...\\n${C_RESET}" "$dsthomedir"
 	cp $srcdir/flake.* $dsthomedir/
 }
 
@@ -21,16 +41,16 @@ update_nixpkg() {
 	local flake_key="$dsthomedir/#$USER"
 	export FLAKEKEY=$flake_key
 	local cur_dir_start=$(pwd)
-	printf "cd into home-manager install dir %s...\n" "$dsthomedir"
+	printf "${BOLD_WHITE}cd into home-manager install dir %s...\\n${C_RESET}" "$dsthomedir"
 	cd $dsthomedir
-	printf "Updating cix channels...\n"
+	printf "${BOLD_WHITE}Updating nix channels...${C_RESET}\\n"
 	nix-channel --update
-	printf "Updating nix flake.lock file...\n"
+	printf "${BOLD_WHITE}Updating nix ${C_B_CYAN}flake.lock${BOLD_WHITE} file...${C_RESET}\\n"
 	nix flake update
-	printf "switching home-manager to flake: %s\n" "$flake_key"
+	printf "${BOLD_WHITE}switching home-manager to flake:${C_B_GREEN} %s\\n${C_RESET}" "$flake_key"
 	home-manager switch --flake $flake_key
-	echo $flake_key
-	printf "changing directory back to the starting dir... %s\n" "$cur_dir_start"
+	printf "${BOLD_WHITE}completed${C_GREEN}flake_key: ${C_B_GREEN}${flake_key}${C_RESET}\\n"
+	printf "${BOLD_WHITE}changing directory back to the starting dir:${C_B_BLUE} %s\\n${C_RESET}" "$cur_dir_start"
 	cd $cur_dir_start
 }
 
@@ -40,12 +60,12 @@ hm_switch() {
 	local cur_dir_start=$(pwd)
 
 	export FLAKEKEY=$flake_key
-	echo "flake key is: $FLAKEKEY"
-	printf "cd into home-manager install dir %s...\n" "$dsthomedir"
+	echo "${BOLD_WHITE}flake key is: ${C_B_GREEN}$FLAKEKEY${C_RESET}"
+	printf "${BOLD_WHITE}cd into home-manager install dir ${C_B_YELLOW}%s...\\n${C_RESET}" "$dsthomedir"
 	cd $dsthomedir
-	printf "switching home-manager to flake: %s\n" "$flake_key"
+	printf "${BOLD_WHITE}switching home-manager to ${C_B_GREEN}flake: %s\\n${C_RESET}" "$flake_key"
 	home-manager switch --flake $flake_key
-	printf "changing directory back to the starting dir... %s\n" "$cur_dir_start"
+	printf "${BOLD_WHITE}changing directory back to the starting dir:${C_B_BLUE} %s\\n${C_RESET}" "$cur_dir_start"
 	cd $cur_dir_start
 }
 
